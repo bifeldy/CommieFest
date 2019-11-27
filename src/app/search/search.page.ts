cdf7098f93cc9cbfdcd504886aaff072d692fef8
stevehart99 <steveaja21@gmail.com>
2019-11-27 10:14:27 +0700

    ...

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -30,8 +30,8 @@ export class SearchPage implements OnInit {
     this.eventService.getEvents().subscribe(res => {
       this.events = res;
       // this.loadedEvent = res;
-      // this.filterData = this.events;
-      // this.setFilter();
+      this.filterData = this.events;
+      this.setFilter();
     });
 
     // Get Data From URL
@@ -44,44 +44,44 @@ export class SearchPage implements OnInit {
   // initItem(): void{
   //   this.events = this.loadedEvent;
   // }
-//   filterList(){
-//     this.initItem();
-//     if(!this.searchQuery){
-//       return;
-//     }
-//     this.events = this.events.filter(currentName=>{
-//     //   if(currentName.name && this.searchQuery){
-//     //     if(currentName.name.toLowerCase().indexOf(this.searchQuery.toLowerCase())>-1){
-//     //       return true;
-//     //     }
-//     //     return false;
-//     //   }
-//     // })
-//     this.filterData = this.events.filter((Event)=>{
-//       return Event.name.toLowerCase().indexOf(this.searchQuery.toLowerCase())>-1;
-//     });
-//   }
+  //   filterList(){
+  //     this.initItem();
+  //     if(!this.searchQuery){
+  //       return;
+  //     }
+  //     this.events = this.events.filter(currentName=>{
+  //     //   if(currentName.name && this.searchQuery){
+  //     //     if(currentName.name.toLowerCase().indexOf(this.searchQuery.toLowerCase())>-1){
+  //     //       return true;
+  //     //     }
+  //     //     return false;
+  //     //   }
+  //     // })
+  //     this.filterData = this.events.filter((Event)=>{
+  //       return Event.name.toLowerCase().indexOf(this.searchQuery.toLowerCase())>-1;
+  //     });
+  //   }
 
-// }
+  // }
 
-  // search($event) {
-  //   this.router.navigateByUrl('/search?q=' + $event.target.value);
-  //   // this.eventService.getEvents().subscribe(res => {
-  //   //   this.events = res;
-  //   //   // this.loadedEvent = res;
-  //   //   // this.filterData = this.events;
-  //   //   // this.setFilter();
-  //   // });
-  // }
-  // setFilter() {
-  //   this.filterData = this.events.filter((event) => {
-  //     return event.name.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1;
-  //   });
-  // }
-  // setFilteredEvents() {
-  //   this.router.navigateByUrl('/search' );
-  //   this.filterData = this.events.filter((event) => {
-  //     return event.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
-  //   });
-  // }
+  search($event) {
+    this.router.navigateByUrl('/search?q=' + $event.target.value);
+    // this.eventService.getEvents().subscribe(res => {
+    //   this.events = res;
+    //   // this.loadedEvent = res;
+    //   // this.filterData = this.events;
+    //   // this.setFilter();
+    // });
+  }
+  setFilter() {
+    this.filterData = this.events.filter((event) => {
+      return event.name.toLowerCase().indexOf(this.searchQuery.toLowerCase()) > -1;
+    });
+  }
+  setFilteredEvents() {
+    this.router.navigateByUrl('/search');
+    this.filterData = this.events.filter((event) => {
+      return event.name.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+    });
+  }
 }
