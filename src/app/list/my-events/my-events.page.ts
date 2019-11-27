66d7ea895a5f4f4034fa3a2e69e8918f1addd7f8
stevehart99 <steveaja21@gmail.com>
2019-11-27 17:41:18 +0700

    Nambah Alert di myevent

--- src/app/list/my-events/my-events.page.ts
+++ src/app/list/my-events/my-events.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService, Event } from 'src/app/_shared/_services/event.service';
-
-import { IonItemSliding } from '@ionic/angular';
+// import {  } from 'ionic-angular';
+import { IonItemSliding, AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-my-events',
   templateUrl: './my-events.page.html',
@@ -9,7 +9,8 @@ import { IonItemSliding } from '@ionic/angular';
 })
 export class MyEventsPage implements OnInit {
 
-  constructor(private eventSvc: EventService) { }
+  constructor(private eventSvc: EventService,
+    private alertCtrl: AlertController) { }
 
   nearbyEvents: Event[] = [];
 
@@ -17,9 +18,28 @@ export class MyEventsPage implements OnInit {
     this.eventSvc.getEvents().subscribe(res => { this.nearbyEvents = res; });
   }
 
-  remove(e, slidingItem: IonItemSliding) {
-    slidingItem.close();
-    this.eventSvc.removeBike(e.id);
+  async remove(e, slidingItem: IonItemSliding) {
+    const alert = await this.alertCtrl.create({
+      header: 'Hapus Event',
+      message: 'Yakin ingin hapus?',
+      buttons: [
+        {
+          text: 'Batal',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Yakin',
+          handler: () => {
+            slidingItem.close();
+            this.eventSvc.removeBike(e.id);
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
